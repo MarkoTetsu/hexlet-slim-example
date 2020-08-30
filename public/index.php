76d9dd8796df09b8eda07a49b1fa7c4512d72322
MarkoTetsu <marko.fero@gmail.com>
2020-08-30 19:06:16 +0300

    Add templates

--- public/index.php
+++ public/index.php
@@ -4,14 +4,53 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
     $response->getBody()->write('Welcome to Slim!');
+    headers($request);
     return $response;
     // Благодаря пакету slim/http этот же код можно записать короче
     // return $response->write('Welcome to Slim!');
 });
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    headers($request);
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
+    // $this доступен внутри анонимной функции благодаря https://php.net/manual/ru/closure.bindto.php
+    // $this в Slim это контейнер зависимостей
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+});
+
+$app->post('/users', function ($request, $response) {
+    headers($request);
+    return $response->withStatus(302);
+});
+
+$app->get('/courses/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
+    headers($request);
+    return $response->write("Course id: {$id}");
+});
+
+function headers($request) {
+    $headers = $request->getHeaders();
+    $uri = $request->getUri();
+    $method = $request->getMethod();
+    echo "<p>{$method} {$uri}</p>"; 
+    foreach ($headers as $name => $values) {
+        echo "<p>" . $name . ": " . implode(", ", $values) . "</p>";
+    }
+}
+
 $app->run();
