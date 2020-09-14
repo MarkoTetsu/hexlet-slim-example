f07c679267c77341602e4d2721af62bbba616cd1
MarkoTetsu <marko.fero@gmail.com>
2020-09-14 20:18:15 +0300

    Added flash messages, user profile update and delete

--- public/index.php
+++ public/index.php
@@ -5,17 +5,45 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
 use DI\Container;
+use Slim\Middleware\MethodOverrideMiddleware;
 
 $container = new Container();
 $container->set('renderer', function () {
     // Параметром передается базовая директория в которой будут храниться шаблоны
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 });
+$container->set('flash', function () {
+    return new Slim\Flash\Messages();
+});
 
 $app = AppFactory::createFromContainer($container);
+
+$app->add(MethodOverrideMiddleware::class);
 $app->addErrorMiddleware(true, true, true);
 
 $router = $app->getRouteCollector()->getRouteParser();
+
+session_start();
+
+$app->get('/', function ($request, $response) {
+    return $this->get('renderer')->render($response, 'index.phtml');
+});
+
+$app->get('/users', function ($request, $response) {
+    $flash = $this->get('flash')->getMessages();
+    $users = readFromFile('users');
+    $term = $request->getQueryParam('term');
+    if ($term !== null) {
+        $filteredUsers = array_filter($users, fn($user) => stripos($user['name'], $term) !== false);
+    } else {
+        $filteredUsers = $users;
+    }
+    $params = [
+        'users' => $filteredUsers,
+        'flash' => $flash
+    ];
+    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
+})->setName('users');
 
 $app->get('/users/new', function ($request, $response) {
     $params = [
@@ -25,18 +53,6 @@ $app->get('/users/new', function ($request, $response) {
     return $this->get('renderer')->render($response, 'users/new.phtml', $params);
 })->setName('newUser');
 
-$app->get('/users', function ($request, $response) {
-    $users = readFromFile('users');
-    $term = $request->getQueryParam('term');
-    if ($term !== null) {
-        $filteredUsers = array_filter($users, fn($user) => stripos($user['name'], $term) !== false);
-    } else {
-        $filteredUsers = $users;
-    }
-    $params = ['users' => $filteredUsers];
-    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
-})->setName('users');
-
 $app->post('/users', function ($request, $response) use ($router) {
     $user = $request->getParsedBodyParam('user');
     $errors = validate($user);
@@ -44,6 +60,7 @@ $app->post('/users', function ($request, $response) use ($router) {
     $userIDExists = in_array($user['id'], array_column($users, 'id'));
     if (count($errors) === 0 && !$userIDExists) {
         saveInFile($user);
+        $this->get('flash')->addMessage('success', 'User profile has been created.');
         $url = $router->urlFor('users');
         return $response->withRedirect($url, 302);
     }
@@ -58,19 +75,73 @@ $app->post('/users', function ($request, $response) use ($router) {
 });
 
 $app->get('/users/{id}', function ($request, $response, $args) {
+    $flash = $this->get('flash')->getMessages();
     $id = htmlspecialchars($args['id']);
     $users = readFromFile('users');
-    $filteredUser = array_values(array_filter($users, fn($user) => stripos($user['id'], $id) !== false));
+    $filteredUser = array_values(array_filter($users, fn($user) => $user['id'] === $id));
     if (empty($filteredUser)) {
         return $response->write('Page not found')->withStatus(404);
     }
     [$user] = $filteredUser;
-    $params = ['user' => $user];
+    $params = [
+        'user' => $user,
+        'flash' => $flash
+    ];
     return $this->get('renderer')->render($response, 'users/user.phtml', $params);
+})->setName('userProfile');
+
+$app->get('/users/{id}/edit', function ($request, $response, $args) {
+    $id = $args['id'];
+    $users = readFromFile('users');
+    $filteredUser = array_values(array_filter($users, fn($user) => $user['id'] === $id));
+    if (empty($filteredUser)) {
+        return $response->write('Page not found')->withStatus(404);
+    }
+    [$user] = $filteredUser;
+    $params = [
+        'user' => $user,
+        'errors' => []
+    ];
+    return $this->get('renderer')->render($response, 'users/edit.phtml', $params);
+})->setName('editUser');
+
+$app->patch('/users/{id}', function ($request, $response, $args) use ($router) {
+    $id = $args['id'];
+    $users = readFromFile('users');
+    $filteredUser = array_filter($users, fn($user) => $user['id'] === $id);
+    $key = array_key_first($filteredUser);
+    $updatedUser = $request->getParsedBodyParam('user');
+    $errors = validate($updatedUser);
+
+    if (count($errors) === 0) {
+        $users[$key]['id'] = $updatedUser['id'];
+        $users[$key]['name'] = $updatedUser['name'];
+        $users[$key]['email'] = $updatedUser['email'];
+
+        $this->get('flash')->addMessage('success', 'User profile has been updated');
+        rewriteFile($users);
+        
+        $url = $router->urlFor('userProfile', ['id' => $users[$key]['id']]);
+        return $response->withRedirect($url);
+    }
+    
+    $params = [
+        'user' => $filteredUser,
+        'errors' => $errors
+    ];
+    $response = $response->withStatus(422);
+    return $this->get('renderer')->render($response, 'users/edit.phtml', $params);
 });
 
-$app->get('/', function ($request, $response) use ($router) {
-    return $this->get('renderer')->render($response, 'index.phtml');
+$app->delete("/users/{id}", function ($request, $response, $args) use ($router) {
+    $id = $args['id'];
+    $users = readFromFile('users');
+    $filteredUser = array_values(array_filter($users, fn($user) => $user['id'] === $id));
+    $users = deleteUser($users, $filteredUser);
+    rewriteFile($users);
+    $this->get('flash')->addMessage('success', 'User profile has been deleted');
+    $url = $router->urlFor('users');
+    return $response->withRedirect($url);
 });
 
 function validate($user)
@@ -88,12 +159,20 @@ function validate($user)
     return $errors;
 }
 
-function saveInFile($user): void
+function saveInFile(array $user): void
 {
     file_put_contents('users', json_encode($user) . '/', FILE_APPEND);
 }
 
-function readFromFile($path)
+function rewriteFile(array $users): void
+{
+    file_put_contents('users', '');
+    foreach ($users as $user) {
+        file_put_contents('users', json_encode($user) . '/', FILE_APPEND);
+    }
+}
+
+function readFromFile(string $path)
 {
     $users = [];
     $fileData = explode('/', file_get_contents($path));
@@ -103,7 +182,30 @@ function readFromFile($path)
         }
     }
     uasort($users, fn($a, $b) => strcmp($a['name'], $b['name']));
+    return array_values($users);
+}
+
+function updateUser(array $users, array $user)
+{
+    foreach ($users as $key => $u) {
+        if ($u['id'] === $user['id']) {
+            $users[$key]['name'] = $user['name'];
+            $users[$key]['body'] = $user['body'];
+            break;
+        }
+    }
     return $users;
+}
+
+function deleteUser(array $users, array $user)
+{
+    foreach ($users as $key => $u) {
+        if ($u['id'] === $user[0]['id']) {
+            unset($users[$key]);
+            break;
+        }
+    }
+    return array_values($users);
 }
 
 $app->run();
